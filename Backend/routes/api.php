8c1f6f2d884a4b24359edfb0a4d3b546c1fd5bc5
Berfin4615 <berfins.4615@gmail.com>
2025-11-21 23:59:59 +0300

    AuthController problem is fixed.

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -1,17 +1,22 @@
 <?php
 
-use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
 
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login',    [AuthController::class, 'login']);
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+*/
+
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login',    [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('logout', [AuthController::class, 'logout']);
 
-    // Test için: token ile kendini döner
-    Route::get('/me', function (Request $request) {
+    Route::get('me', function (Request $request) {
         return $request->user();
     });
 });
